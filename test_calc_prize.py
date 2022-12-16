a946945c8891e6ccba1e56dc96b400eda08a705b
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-16 10:58:52 +0100

    Change test

--- test_calc_prize.py
+++ test_calc_prize.py
@@ -3,12 +3,13 @@ from api import calculate_prize_amount
 
 
 TEST_DATA = [(100, "1/2", 50),
-             (100, "1/3", 33.33),
+             (100, "1/3", 33.333),
              (0, "1/4", 0),
              (100, "1/1", 100),
-             (100, "1/100", 1)]
+             (100, "1/100", 1),
+             (10000000000000, "1/3", 3333333333333.333)]
 
 
 @pytest.mark.parametrize("prize_amount, portion_str, expected", TEST_DATA)
 def test_calculate_prize_amount(prize_amount: int, portion_str: str, expected: float):
-    assert calculate_prize_amount(prize_amount, portion_str) == pytest.approx(expected, 0.001)
+    assert calculate_prize_amount(prize_amount, portion_str) == pytest.approx(expected, abs=0.001)
