3c623a638291dd70b52d6f60feb152b8dc0917cd
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-16 09:51:31 +0100

    Change known_name lookup

--- api.py
+++ api.py
@@ -19,7 +19,11 @@ def calculate_prize_amount(prize_amount: int, portion_str: str) -> float:
 
 
 def print_laureate(laureate: dict, prize_amount: int, prize_adjusted: int):
-    print(laureate.get("knownName", {}).get("en", "Unknown name"))  # Some laureates don't have a known name
+    # Check if laureate has a known name
+    if "knownName" in laureate:
+        print(laureate["knownName"]["en"])
+    else:
+        print("Unknown name")
     print(laureate['motivation']['en'])
     portion = laureate['portion']
     prize_money = calculate_prize_amount(prize_amount, portion)
